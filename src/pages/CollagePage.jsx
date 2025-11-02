3bbc715f3321c7c128c9ddc72a4e522dd1a9c650
ShubhamMisal07 <shubhamsunilmisal07@gmail.com>
2025-11-02 17:22:40 +0530

    Added collage image222

--- src/pages/CollagePage.jsx
+++ src/pages/CollagePage.jsx
@@ -2,7 +2,7 @@ const CollagePage = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center bg-black">
       <img
-        src="/collage.jpg"
+        src="/Collage.jpg"
         alt="Collage"
         className="object-contain h-full w-full"
       />
